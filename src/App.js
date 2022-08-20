4a8f1a656ec7494a12a97f818fe70648f85e6fc7
quangnhd <quangnguyen10893@gmail.com>
2022-08-20 16:05:13 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RESTORE_USER } from './store/reducer/user/userActionTypes';
 import './App.css';
+import Cookies from 'universal-cookie';
+import NavigationBar from './layout/NavigationBar/NavigationBar';
+import { Route, Routes } from 'react-router';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import IdeaDetail from './pages/IdeaDetail/IdeaDetail';
 
-function App() {
+const cookies = new Cookies();
+
+const App = () => {
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  if (!user) {
+    const prevUser = cookies.get('user');
+    if (prevUser) {
+      dispatch({ type: RESTORE_USER, payload: prevUser });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavigationBar></NavigationBar>
+      <div className="app-route-container">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/ideas/:id"
+            element={
+              <ProtectedRoute condition={user}>
+                <IdeaDetail />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute condition={user}>
+                <IdeaDetail />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
